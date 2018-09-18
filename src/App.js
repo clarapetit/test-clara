e09c167db7e0de60293d9e06c1bdc3f2a8779d5f
Clara Petit <clara.petit@shopify.com>
2018-09-18 17:30:34 -0400

    dev-clara

--- src/App.js
+++ src/App.js
@@ -1,44 +1,150 @@
 import React, { Component } from 'react';
-import {AppProvider, Page, Card, Button, Tooltip, Layout} from '@shopify/polaris';
+import { AppProvider, Page, Card, Button, Tooltip, Layout, ResourceListExample, ResourceList, TextStyle, FilterType, Pagination, Stack } from '@shopify/polaris';
 import '@shopify/polaris/styles.css';
 import './App.css';
 
 class App extends Component {
+  state = {
+    searchValue: '',
+    appliedFilters: [],
+    selectedItems: [],
+  };
 
-  state={loading:false}
+  handleSelectionChange = (selectedItems) => {
+    this.setState({selectedItems});
+  };
 
-  onClick() {
-    this.setState({
-      loading:true
-    })
+  handleSearchChange = (searchValue) => {
+    this.setState({searchValue});
+  };
 
-    setTimeout(() => {
-      this.setState({
-        loading:false
-      })
-    }, 2000)
-  }
+  handleFiltersChange = (appliedFilters) => {
+    this.setState({appliedFilters});
+  };
+
+  renderItem = (item) => {
+    const {id, url, name, state, openOrderURL} = item;
+    const shortcutActions = openOrderURL
+        ?[{content: 'Open', url: openOrderURL}]
+        : null;
+
+    return (
+      <ResourceList.Item
+        id={id}
+        url={url}
+        shortcutActions={shortcutActions}>
+        <h3>
+          <TextStyle variation="strong">{name}</TextStyle>
+        </h3>
+        <div>{state}</div>
+      </ResourceList.Item>
+    );
+  };
 
   render() {
+    const resourceName = {
+      singular: 'return',
+      plural: 'returns',
+    };
+
+    const items = [
+      {
+        id: 341,
+        url: 'returns/341347',
+        name: '341347',
+        state: 'Received',
+        openOrderURL: 'returns/341347',
+      },
+      {
+        id: 256,
+        url: 'returns/256892',
+        name: '256892',
+        state: 'Approved',
+        openOrderURL: 'returns/256892'
+      },
+    ];
+
+    const filters = [
+      {
+        key: 'orderNumberFilter',
+        label: 'Order Number',
+        operatorText: 'is',
+        type: FilterType.TextField,
+      },
+      {
+        key: 'returnStatusFilter',
+        label: 'Return status',
+        operatorText: 'is',
+        type: FilterType.Select,
+        options: ['Approved', 'Received', 'Controlled', 'Processed', 'Deleted', 'Rejected', 'Archived'],
+      },
+    ];
+
+    const filterControl = (
+      <ResourceList.FilterControl
+        filters={filters}
+        appliedFilters={this.state.appliedFilters}
+        onFiltersChange={this.handleFiltersChange}
+        searchValue={this.state.searchValue}
+        onSearchChange={this.handleSearchChange}
+        additionalAction={{
+          content: 'Save',
+          onAction: () => console.log('New filter saved'),
+        }}
+      />
+    );
+
+    const promotedBulkActions = [
+      {
+        content: 'Edit returns',
+        onAction: () => console.log('Todo: implement bulk edit'),
+      },
+    ];
+
+    const bulkActions = [
+      {
+        content: 'Add tags',
+        onAction: () => console.log('Todo: implement bulk add tags'),
+      },
+      {
+        content: 'Remove tags',
+        onAction: () => console.log('Todo: implement bulk remove tags'),
+      },
+      {
+        content: 'Delete returns',
+        onAction: () => console.log('Todo: implement bulk delete'),
+      },
+    ];
+
     return (
       <AppProvider>
-    <Page title="Example app">
-      <Layout>
-        <Layout.Section>
-          <Card title='Order details' sectioned>
-            <p>View a summary of your order.</p>
-            <br />
-            <Button onClick={() => this.onClick()} loading={this.state.loading}>View Summary</Button>
-          </Card>
-        </Layout.Section>
-          <Layout.Section secondary>
-            <Card title='Tags' sectioned>
-              <p>Add tags to your order.</p>
-            </Card>
-          </Layout.Section>
-        </Layout>
-    </Page>
-  </AppProvider>
+        <Page title="Returns">
+        <Card>
+          <ResourceList
+            resourceName={resourceName}
+            items={items}
+            renderItem={this.renderItem}
+            filterControl={filterControl}
+            selectedItems={this.state.selectedItems}
+            onSelectionChange={this.handleSelectionChange}
+            promotedBulkActions={promotedBulkActions}
+            bulkActions={bulkActions}
+          />
+          <Stack distribution='center'>
+          <Pagination>
+            hasPrevious
+              onPrevious={() => {
+                console.log('Previous');
+              }}
+            hasNext
+              onNext={() => {
+                console.log('Next');
+              }}
+          </Pagination>
+          </Stack>
+        </Card>
+        </Page>
+      </AppProvider>
     );
   }
 }
